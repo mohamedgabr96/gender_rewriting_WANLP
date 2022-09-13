2880ec17eb56b2b602fec4e66a68a1fe9f7e1271
Mohamed Gabr <mohamed.gabr@hotmail.com>
2022-09-13 07:53:14 +0000

    Adding speaker listener classifier modules

---
+++ speaker_listener_classifier/main.py
@@ -0,0 +1,161 @@
+import torch
+from transformers import AutoModelForTokenClassification, AutoTokenizer
+from argparse import Namespace
+import argparse
+import os
+import sys
+sys.path.append("../")
+from utils_speaker_listener import create_dataloaders, yaml_loader
+import wandb
+from tqdm import tqdm, trange
+
+def main(config):
+    ### Dataloading
+
+    ## Model Loading
+    wandb.init(project="my-gender-rewrite-project-speaker_listener",
+            name=f"experiment_{config.exp_name}", 
+            # Track hyperparameters and run metadata
+            config={
+            "learning_rate": config.learning_rate,
+            "model_name": config.model_name,
+            "speak_listen_mode": config.speak_listen_mode,
+            "max_length": config.max_length,
+            "batch_size": config.batch_size,
+            "eval_every": config.eval_every,
+            "epochs": config.no_epochs,
+            })
+
+
+
+    tokenizer = AutoTokenizer.from_pretrained(config.model_name, use_fast=False)
+
+    train_dataloader, dev_dataloader, test_dataloader = create_dataloaders(config, tokenizer)
+
+    label_names = ["NONE", "SPEAKER", "LISTENER", "PAD"]
+    id2label = {str(i): label for i, label in enumerate(label_names)}
+    label2id = {v: k for k, v in id2label.items()}
+
+    model = AutoModelForTokenClassification.from_pretrained(config.model_name, id2label=id2label, label2id=label2id).to(config.device)
+
+    ## Criterion and Schedulers
+  
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
+
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer)
+
+
+    no_steps = 0
+    model.train()
+    ## training Loop
+    previous_dev_loss = 10000
+    for epoch in range(config.no_epochs):
+        with tqdm(train_dataloader, unit="batch") as tepoch:
+            for batch in tepoch:
+                tepoch.set_description(f"Epoch {epoch}")
+                training_loss_val = training_loop(model, batch, optimizer, scheduler)
+                no_steps+=1
+
+                wandb.log({"Training Loss": training_loss_val})
+
+                if no_steps % config.eval_every == 0:
+                    dev_loss_val, accuracy, accuracy_sl_only = eval_loop(model, dev_dataloader, tokenizer)
+                    wandb.log({"Dev Loss": dev_loss_val})
+                   
+                    wandb.log({"Accuracy": accuracy, "Accuracy Speaker Listener": accuracy_sl_only})
+
+                    if dev_loss_val < previous_dev_loss:
+                        save_model(model, config.model_saves_path + "_" + config.exp_name, no_steps)
+                        
+                    previous_dev_loss = dev_loss_val
+
+
+                tepoch.set_postfix(loss=training_loss_val)
+        
+
+
+    print("Done")
+
+def training_loop(model, batch, optimizer, scheduler):
+
+    optimizer.zero_grad()
+
+    model_output = model(input_ids=batch[0], labels=batch[1])
+
+    loss = model_output.loss
+
+    loss.backward()
+    optimizer.step()
+
+    return loss.item()
+
+def eval_loop(model, dev_loader, tokenizer, generate_sents=False):
+    model.eval()
+    
+    no_batches = 0
+    accum_loss = 0
+
+    total_correct_labels = 0
+    total_labels =  0
+
+    total_correct_labels_sl_only = 0
+    total_labels_sl_only =  0
+
+    for batch in tqdm(dev_loader): 
+        model_output = model(input_ids=batch[0], labels=batch[1])
+        no_batches += 1
+        accum_loss += model_output.loss.item()
+        
+        logits_argmax = torch.argmax(model_output.logits, axis=2)
+        labels=batch[1]
+
+        ## With the None
+        labels_to_attend_on = labels != 3
+        correct_labels = labels_to_attend_on & (logits_argmax == labels)
+        no_correct_labels = int(sum(sum(correct_labels.to(int))).cpu().detach().numpy())
+        no_labels = int(sum(sum(labels_to_attend_on.to(int))).cpu().detach().numpy())
+
+        total_correct_labels += no_correct_labels
+        total_labels += no_labels
+
+        # Without the None
+        labels_to_attend_on = (labels != 3) & (labels != 0)
+        correct_labels = labels_to_attend_on & (logits_argmax == labels)
+        no_correct_labels_sl_only = int(sum(sum(correct_labels.to(int))).cpu().detach().numpy())
+        no_labels_sl_only = int(sum(sum(labels_to_attend_on.to(int))).cpu().detach().numpy())
+        
+        total_correct_labels_sl_only += no_correct_labels_sl_only
+        total_labels_sl_only +=  no_labels_sl_only
+
+    accuracy_all = total_correct_labels / total_labels
+    accuracy_sl_only = total_correct_labels_sl_only / total_labels_sl_only
+
+    model.train()
+
+    return accum_loss / no_batches, accuracy_all, accuracy_sl_only
+
+
+def save_model(model, path, step_no):
+    path_with_step = os.path.join(path, "dev_" + str(step_no))
+    if not os.path.exists(path_with_step):
+        os.makedirs(path_with_step)
+
+    model.save_pretrained(path_with_step)
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--config_path',
+            type=str,
+        default=None,
+        help='The path to the config file'
+    )
+
+    args = parser.parse_args()
+
+    config_path = args.config_path
+    config = Namespace(**yaml_loader(config_path))
+    main(config)
---
+++ speaker_listener_classifier/utils_speaker_listener.py
@@ -0,0 +1,144 @@
+import torch
+import os
+import yaml
+from tqdm import tqdm
+
+def yaml_loader(path):
+    with open(path, encoding="utf-8") as yaml_file:
+        yaml_loaded = yaml.load(yaml_file, Loader=yaml.FullLoader)
+
+    return yaml_loaded
+
+
+def load_normal_data_speaker_listener(path, split="train", direction="MF", augmented=False):
+
+    # MM: male speaking to a male
+    # MF: male speaking to a female 
+    # FF: female speaking to a female
+    # FM: female speaking to a male
+
+    augmented_string = ".augmented" if augmented else ""
+
+    with open(os.path.join(path, split, f"{split}.arin.tokens" + augmented_string), encoding="utf-8") as file_opened:
+        source = file_opened.readlines()
+
+    sentences_list_source = []
+    sentence_temp_list = []
+    for line in source:
+        if line == '\n':
+            sentences_list_source.append(sentence_temp_list)
+            sentence_temp_list = []
+        else:
+            temp_line = line.strip("\n").split(" ")
+            if temp_line[2][0] == "1" or temp_line[2][0] == "2":
+                word = temp_line[1]
+                label = int(temp_line[2][0])
+            else:
+                word = temp_line[1]
+                label = 0
+            sentence_temp_list.append([word, label])
+    
+    # source = [x.replace("</s>", "").replace("</s>", "").replace("</s>", "") for x in source]
+
+    with open(os.path.join(path, split, f"{split}.ar.{direction}.tokens" + augmented_string), encoding="utf-8") as file_opened:
+        target = file_opened.readlines()
+
+    sentences_list_target = []
+    sentence_temp_list = []
+    for line in target:
+        if line == '\n':
+            sentences_list_target.append(sentence_temp_list)
+            sentence_temp_list = []
+        else:
+            temp_line = line.strip("\n").split(" ")
+            if temp_line[2][0] == "1" or temp_line[2][0] == "2":
+                word = temp_line[1]
+                label = int(temp_line[2][0])
+            else:
+                word = temp_line[1]
+                label = 0
+            sentence_temp_list.append([word, label])
+    
+    return sentences_list_source, sentences_list_target
+
+
+def tokenize_data(config, data_in, tokenizer, DEBUG=False):
+    new_sentence_list = []
+    count = 0
+    for sentence in tqdm(data_in):
+        count += 1
+        temp_list_sentence = []
+        for word in sentence:
+            word_str = word[0]
+            tokenized_word = tokenizer([word_str])["input_ids"][0][:-1]
+            temp_list_sentence.append([tokenized_word[0], word[1]])
+            for subword in tokenized_word[1:]:
+                temp_list_sentence.append([subword, 3])
+        
+        max_len_truncate = config.max_length if tokenizer.eos_token_id is None else  config.max_length - 1
+        if len(temp_list_sentence) > max_len_truncate:
+            temp_list_sentence = temp_list_sentence[:max_len_truncate]
+            if tokenizer.eos_token_id is not None:
+                temp_list_sentence.append(tokenizer.eos_token_id)
+        else:
+            if tokenizer.eos_token_id is not None:
+                temp_list_sentence.append([tokenizer.eos_token_id, 3])
+            while len(temp_list_sentence) < config.max_length:
+                temp_list_sentence.append([tokenizer.pad_token_id, 3])
+        
+        assert len(temp_list_sentence) == config.max_length
+        new_sentence_list.append(temp_list_sentence)
+
+        if DEBUG and count > 100:
+            return new_sentence_list
+
+    return new_sentence_list
+
+
+def create_dataloader(config, tokenizer, split="train"):
+    source_data, target_data_MM = load_normal_data_speaker_listener(config.data_path, split=split, direction="MM")
+    _, target_data_MF = load_normal_data_speaker_listener(config.data_path, split=split, direction="MF")
+    _, target_data_FM = load_normal_data_speaker_listener(config.data_path, split=split, direction="FM")
+    _, target_data_FF = load_normal_data_speaker_listener(config.data_path, split=split, direction="FF")
+
+    all_data_combined = source_data + target_data_MM + target_data_MF + target_data_FM + target_data_FF
+
+    tokenized_data = tokenize_data(config, all_data_combined, tokenizer, DEBUG=False)
+    dataset = SpeakerListenerDataset(config, tokenized_data)
+
+    params = {'batch_size': config.batch_size,
+          'shuffle': True}
+
+    dataloader = torch.utils.data.DataLoader(dataset, **params)
+
+    return dataloader
+
+
+class SpeakerListenerDataset(torch.utils.data.Dataset):
+  'Characterizes a dataset for PyTorch'
+  def __init__(self, config, tokenized_data):
+        'Initialization'
+        self.tokens_ids = torch.tensor([[x[0] for x in y] for y in tokenized_data]).to(config.device)
+        self.labels_ids = torch.tensor([[x[1] for x in y] for y in tokenized_data]).to(config.device)
+
+        assert self.tokens_ids.shape[0] == self.labels_ids.shape[0]
+        assert self.tokens_ids.shape[1] == self.labels_ids.shape[1]
+
+  def __len__(self):
+        'Denotes the total number of samples'
+        return len(self.tokens_ids)
+
+  def __getitem__(self, index):
+        'Generates one sample of data'
+
+        return self.tokens_ids[index], self.labels_ids[index]
+
+
+def create_dataloaders(config, tokenizer):
+    train_dataloader = create_dataloader(config, tokenizer)
+    dev_dataloader = create_dataloader(config, tokenizer, split="dev")
+    test_dataloader = create_dataloader(config, tokenizer, split="test")
+
+    return train_dataloader, dev_dataloader, test_dataloader
+
+
